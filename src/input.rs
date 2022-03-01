36c14396bcc78858572262b1da0af6fee8a30065
nils-mathieu <nils.mathieu.contact@gmail.com>
2022-03-01 19:26:52 +0100

    fix: read_line

--- src/input.rs
+++ src/input.rs
@@ -4,5 +4,27 @@ pub fn read_line() -> String {
     std::io::stdin()
         .read_line(&mut result)
         .expect("failed to read from stdin");
+
+    // Trim starting whitespaces.
+    let start = result
+        .char_indices()
+        .find(|(_, c)| !c.is_whitespace())
+        .map(|(i, _)| i)
+        .unwrap_or_default();
+
+    // SAFETY:
+    //  We made sure that `start` is on a character boundary.
+    unsafe { result.as_bytes_mut().copy_within(start.., 0) };
+    result.truncate(result.len() - start);
+
+    // Trim eventual trailing line feed.
+    let end = result
+        .char_indices()
+        .rev()
+        .find(|(_, c)| !c.is_whitespace())
+        .map(|(i, c)| i + c.len_utf8())
+        .unwrap_or(result.len());
+    result.truncate(end);
+
     result
 }
