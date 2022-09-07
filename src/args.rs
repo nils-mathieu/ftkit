759a1099b44a5a6a3f30a66494cce51a19fbe49a
nils.mathieu <nils.mathieu.contact@gmail.com>
2022-09-07 22:08:39 +0200

    fix: improve documentation for `Arguments`

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,24 @@
 use std::ops::Index;
 
 /// Represents the arguments passed to the application.
+///
+/// # Examples
+///
+/// ```
+/// # use ftkit::Arguments;
+/// #
+/// let args = Arguments::from_env();
+///
+/// if args.count() != 0 {
+///     print!("{}", &args[0]);
+///     
+///     for i in 1..args.count() {
+///         print!(" {}", &args[i]);
+///     }
+/// }
+///
+/// println!();
+/// ```
 pub struct Arguments(Box<[Box<str>]>);
 
 impl Arguments {
@@ -19,6 +37,19 @@ impl Arguments {
     ///
     /// If the provided index is out of bounds (greater than [`count`](Arguments::count)), [`None`]
     /// is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ftkit::Arguments;
+    /// #
+    /// let args = Arguments::from_env();
+    ///
+    /// match args.get(1) {
+    ///     Some(arg) => println!("The first argument is '{arg}'"),
+    ///     None => println!("There is no first argument"),
+    /// }
+    /// ```
     #[inline(always)]
     pub fn get(&self, index: usize) -> Option<&str> {
         self.0.get(index).map(Box::as_ref)
