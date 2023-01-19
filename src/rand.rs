30c578c54608ffe2aaa4cf8d871f5957719cbdca
Nils Mathieu <nils.mathieu.contact@gmail.com>
2023-01-19 07:51:42 +0100

    use wyrand instead of getrandom to generate random numbers
    that improved performances by ~x30

--- src/rand.rs
+++ src/rand.rs
@@ -1,4 +1,35 @@
+use std::cell::Cell;
 use std::ops::{Bound, RangeBounds};
+
+thread_local! {
+    /// The state of the global random number generator.
+    ///
+    /// When the value equals `0`, the PRNG has not been initialized yet and its state should not
+    /// be used as a seed.
+    static RAND_STATE: Cell<u64> = Cell::new(0);
+}
+
+/// Generates a pseudo-random `u32` instance.
+///
+/// This internal function do not support bounds.
+fn next_u64() -> u64 {
+    RAND_STATE.with(|state| {
+        if state.get() == 0 {
+            state.set(
+                std::time::SystemTime::UNIX_EPOCH
+                    .elapsed()
+                    .unwrap()
+                    .as_nanos() as u64,
+            );
+        }
+
+        // Credits:
+        //  WyRand: https://github.com/wangyi-fudan/wyhash
+        state.set(state.get().wrapping_add(0xa0761d6478bd642f));
+        let t = (state.get() as u128).wrapping_mul((state.get() ^ 0xe7037ed1a0b428db) as u128);
+        (t.wrapping_shr(64) ^ t) as u64
+    })
+}
 
 /// Generates a random number within the provided bounds.
 ///
@@ -48,14 +79,42 @@ pub fn random_number(range: impl RangeBounds<i32>) -> i32 {
         "can't generate a random number within an empty range"
     );
 
-    let mut bytes = [0u8; 4];
-    getrandom::getrandom(&mut bytes).expect("failed to generate a random number");
-
+    let raw = next_u64() as u32;
     if min == i32::MIN && max == i32::MAX {
-        i32::from_ne_bytes(bytes)
+        raw as i32
     } else {
-        u32::from_ne_bytes(bytes)
-            .wrapping_rem((max as u32).wrapping_sub(min as u32).wrapping_add(1))
+        raw.wrapping_rem((max as u32).wrapping_sub(min as u32).wrapping_add(1))
             .wrapping_add(min as u32) as i32
+    }
+}
+
+#[cfg(test)]
+mod random_number {
+    use super::random_number;
+
+    #[test]
+    fn range() {
+        let mut found = [false; 10];
+
+        for _ in 0..1000 {
+            found[random_number(0..10) as usize] = true;
+        }
+
+        for (i, f) in found.iter().enumerate() {
+            assert!(f, "{i} was never generated");
+        }
+    }
+
+    #[test]
+    fn range_inclusive() {
+        let mut found = [false; 10];
+
+        for _ in 0..1000 {
+            found[random_number(0..10) as usize] = true;
+        }
+
+        for (i, f) in found.iter().enumerate() {
+            assert!(f, "{i} was never generated");
+        }
     }
 }
