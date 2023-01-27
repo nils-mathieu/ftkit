f80a053197998f408d469fc5fc5c94eeffdd9b81
Nils Mathieu <nils.mathieu.contact@gmail.com>
2023-01-27 16:03:41 +0100

    update the read_line and read_number functions

--- src/input.rs
+++ src/input.rs
@@ -1,52 +1,41 @@
 /// Reads a single line from the standard input.
 ///
+/// The terminating `\n` character is preserved, but will be absent on end of input.
+///
 /// # Panics
 ///
 /// This function panics if an error occurs whilst reading the standard input of the program.
 ///
 /// # Examples
 ///
+/// Reading a single line:
+///
 /// ```no_run
 /// let line = ftkit::read_line();
-/// println!("You just wrote: {line}");
+/// println!("You just wrote: {}", line.trim());
+/// ```
+///
+/// Reading lines until the End-Of-File:
+///
+/// ```no_run
+/// loop {
+///     let line = ftkit::read_line();
+///     if line.is_empty() {
+///         break;
+///     }
+///     println!("You just wrote: {}", line.trim());
+/// }
 /// ```
 pub fn read_line() -> String {
     let mut result = String::new();
     std::io::stdin()
         .read_line(&mut result)
         .expect("failed to read from stdin");
-
-    // Trim starting whitespaces.
-    let start = result
-        .find(|c: char| !c.is_whitespace())
-        .unwrap_or_default();
-
-    // SAFETY:
-    //  We made sure that `start` is on a character boundary.
-    unsafe { result.as_bytes_mut().copy_within(start.., 0) };
-    result.truncate(result.len() - start);
-
-    // Trim eventual trailing line feed.
-    let mut end = result
-        .rfind(|c: char| !c.is_whitespace())
-        .unwrap_or(result.len());
-    // `rfind` returns the index of the first maching character. What we want is the index of the
-    // first non-maching character. For this reason, we have to find the length of the character
-    // and add it to the result.
-    match result.as_bytes().get(end) {
-        Some(&b) if b & 0x80 == 0x00 => end += 1, // byte starts with 0b0
-        Some(&b) if b & 0xe0 == 0xc0 => end += 2, // byte starts with 0b110
-        Some(&b) if b & 0xf0 == 0xe0 => end += 3, // byte starts with 0b1110
-        Some(&b) if b & 0xf8 == 0xf0 => end += 4, // byte starts with 0b11110
-        _ => (), // Either the data is not valid UTF-8 or we reached the end of the string.
-    }
-    result.truncate(end);
-
     result
 }
 
 /// Reads a number from the standard input. The function loops indefinitely until a valid number is
-/// provided.
+/// provided. If the End-Of-File is reached, the function panics.
 ///
 /// # Panics
 ///
@@ -61,7 +50,9 @@ pub fn read_line() -> String {
 /// ```
 pub fn read_number() -> i32 {
     loop {
-        if let Ok(val) = read_line().parse() {
+        let s = read_line();
+        assert!(!s.is_empty(), "EOF reached :(");
+        if let Ok(val) = s.trim().parse() {
             break val;
         }
     }
